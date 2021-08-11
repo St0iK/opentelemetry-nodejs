d2c3ffbfe04e0d89d58de56b918606bdc0130567
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2021-08-11 11:24:18 +0100

    add redis

--- src/mars-service/src/middleware/photo.js
+++ src/mars-service/src/middleware/photo.js
@@ -1,7 +1,19 @@
 import NasaApi from "../services/nasa-api";
 import NasaApiConfig from "../config/nasa-api-config"
+const redis = require('redis');
+
 
 export default async (ctx) => {
+  const client = redis.createClient();
+
+  client.on("error", function(error) {
+    console.error(error);
+  });
+
+  client.set("key", "value", redis.print);
+  client.get("key", redis.print);
+
+
   console.log(NasaApiConfig);
   const api = new NasaApi(NasaApiConfig);
   const res = await api.getLatestPhotos();
